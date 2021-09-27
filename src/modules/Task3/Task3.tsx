17ac45a7d3026da843a151a7d5ebfee47440358a
zelo2000 <sashko.zel2000@gmail.com>
2021-09-27 23:23:42 +0300

    Add 3.1 and 3.2 tabs

--- src/modules/Task3/Task3.tsx
+++ src/modules/Task3/Task3.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Typography } from "antd";
+import { Tabs, Typography } from "antd";
 
 import PageHeader from "../../components/PageHeader/PageHeader";
 import DurationByInterestRate from './Forms/DurationByInterestRate';
@@ -10,6 +10,7 @@ import RateByYearsDuration from './Forms/RateByYearsDuration';
 import './Task3.css';
 
 const { Title } = Typography;
+const { TabPane } = Tabs;
 
 const Task3: FC = () => {
 	return (
@@ -20,17 +21,25 @@ const Task3: FC = () => {
 			/>
 
 			<div className="page-content">
-				<Title level={5}>Тривалість періоду на основі простої відсоткової ставки</Title>
-				<DurationByInterestRate />
+				<Tabs defaultActiveKey="1">
+					<TabPane tab="3.1. Визначення деяких параметрів фінансових угод з простими ставками" key="1">
+						<Title level={5}>Тривалість періоду на основі простої відсоткової ставки</Title>
+						<DurationByInterestRate />
 
-				<Title level={5}>Тривалість періоду на основі облікової ставки</Title>
-				<DurationByDiscountRate />
+						<Title level={5}>Тривалість періоду на основі облікової ставки</Title>
+						<DurationByDiscountRate />
 
-				<Title level={5}>Облікова та проста відсоткова ставка на основі тривалості періоду в роках</Title>
-				<RateByYearsDuration />
+						<Title level={5}>Облікова та проста відсоткова ставка на основі тривалості періоду в роках</Title>
+						<RateByYearsDuration />
 
-				<Title level={5}>Облікова та проста відсоткова ставка на основі тривалості періоду в днях</Title>
-				<RateByDaysDuration />
+						<Title level={5}>Облікова та проста відсоткова ставка на основі тривалості періоду в днях</Title>
+						<RateByDaysDuration />
+					</TabPane>
+
+					<TabPane tab="3.2. Визначення деяких параметрів фінансових угод з складними ставками" key="2">
+
+					</TabPane>
+				</Tabs>
 			</div>
 		</div>
 	);
