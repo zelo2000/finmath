802c667e924cd7bc8b32bf0e8db489b635935e99
Yaryna <p.yaryna@gmail.com>
2021-10-03 12:40:52 +0300

    add base navigation to tasks 1 and 2

--- src/modules/Task1/Task1.tsx
+++ src/modules/Task1/Task1.tsx
@@ -1,10 +1,55 @@
 import React, { FC } from "react";
+import { Tabs, Typography } from "antd";
 import PageHeader from "../../components/PageHeader/PageHeader";
+import SimpleRateFormula from "./SimpleRatesForm/SimpleRateFormula";
+import RateCalulationMethods from "./SimpleRatesForm/RateCalulationMethods";
+import CustomCredit from "./СustomСredit/CustomCredit";
+import MathematicalDiscounting from "./DiscountingAndAccounting/MathematicalDiscounting";
+import BankAccounting from "./DiscountingAndAccounting/BankAccounting";
+
+const { Title } = Typography;
+const { TabPane } = Tabs;
 
 const Task1: FC = () => {
 	return (
 		<div>
 			<PageHeader title="Тема 1." description="Прості відсотки" />
+
+			<div className="page-content">
+				<Tabs defaultActiveKey="1">
+					<TabPane tab="1.1. Нарощення за простими відсотковими ставками" key="1">
+						<Title level={5}>Формула простих відсотків</Title>
+						<SimpleRateFormula />
+
+						<Title level={5}>Методи нарахування простих відсотків</Title>
+						<RateCalulationMethods />
+
+						<Title level={5}>Облікова та відсоткова ставка на основі тривалості періоду в роках</Title>
+						<SimpleRateFormula />
+
+						<Title level={5}>Облікова та відсоткова ставка на основі тривалості періоду в днях</Title>
+						<SimpleRateFormula />
+					</TabPane>
+
+					<TabPane tab="1.2. Нарахування простих відсотків на змінні в часі суми депозиту" key="2">
+						<Title level={5}>Тривалість періоду в роках на основі відсоткової ставки</Title>
+
+					</TabPane>
+
+					<TabPane tab="1.3. Нарахування відсотків у користувацькому кредиті" key="3">
+						<CustomCredit />
+					</TabPane>
+
+					<TabPane tab="1.4. Дисконтування та облік за простими відсотковими ставками" key="4">
+						<Title level={5}>Математичне дисконтування</Title>
+						<MathematicalDiscounting />
+
+						<Title level={5}>Банківський облік</Title>
+						<BankAccounting />
+					</TabPane>
+				</Tabs>
+			</div>
+
 		</div>
 	);
 };
--- src/modules/Task2/Task2.tsx
+++ src/modules/Task2/Task2.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from "react";
+import { Tabs, Typography } from "antd";
 import PageHeader from "../../components/PageHeader/PageHeader";
+
+const { Title } = Typography;
+const { TabPane } = Tabs;
 
 const Task2: FC = () => {
 	return (
@@ -8,6 +12,39 @@ const Task2: FC = () => {
 				title="Тема 2."
 				description="Нарощення та дисконтування за складними відсотковими ставками"
 			/>
+
+			<div className="page-content">
+				<Tabs defaultActiveKey="1">
+					<TabPane tab="2.1. Нарощення за складними відсотковими ставками" key="1">
+						<Title level={5}>Тривалість періоду на основі відсоткової ставки</Title>
+
+
+						<Title level={5}>Тривалість періоду на основі облікової ставки</Title>
+
+
+						<Title level={5}>Облікова та відсоткова ставка на основі тривалості періоду в роках</Title>
+
+
+						<Title level={5}>Облікова та відсоткова ставка на основі тривалості періоду в днях</Title>
+
+					</TabPane>
+
+					<TabPane tab="2.2. Математичне дисконтування та облік за складними ставками відсотка" key="2">
+						<Title level={5}>Тривалість періоду в роках на основі відсоткової ставки</Title>
+
+
+						<Title level={5}>Тривалість періоду в роках на основі облікової ставки</Title>
+
+
+						<Title level={5}>Відсоткова ставка на основі тривалість періоду в роках</Title>
+
+
+						<Title level={5}>Облікова ставка на основі тривалість періоду в роках</Title>
+
+					</TabPane>
+				</Tabs>
+			</div>
+
 		</div>
 	);
 };
