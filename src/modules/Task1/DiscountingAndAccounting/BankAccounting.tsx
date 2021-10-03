2ea96a0ce762a5d5536871abba72240c6ffa71d2
Yaryna <p.yaryna@gmail.com>
2021-10-03 17:21:20 +0300

    finish 1.4

--- src/modules/Task1/DiscountingAndAccounting/BankAccounting.tsx
+++ src/modules/Task1/DiscountingAndAccounting/BankAccounting.tsx
@@ -1,12 +1,14 @@
-import React, { FC, useCallback } from "react";
-import { Col, Form, InputNumber, Row } from "antd";
+import React, { FC, useCallback, useState } from "react";
+import { Col, Form, InputNumber, Radio, Row } from "antd";
 
 import { round } from '../../../utils/helpers';
 
-export interface BankAccountingProps {
+export interface BankAccountingProps { // completely done
     initialLoan: number;
     rate: number;
     term: number;
+    daysAmount: number;
+    yearDaysAmount: number;
     paymentCount: number;
     eventualLoan: number;
     discount: number;
@@ -14,14 +16,25 @@ export interface BankAccountingProps {
 
 const BankAccounting: FC = () => {
     const [form] = Form.useForm<BankAccountingProps>();
+    const [showYear, setShowYear] = useState<boolean>(true);
 
     const handleChange = useCallback((_, allValues: BankAccountingProps) => {
-        if (allValues.eventualLoan && allValues.rate && allValues.term) {
-            const initialLoan = allValues.eventualLoan * (1 - allValues.term * (allValues.rate / 100));
+        if (allValues.eventualLoan && allValues.rate && (allValues.term || (allValues.daysAmount && allValues.yearDaysAmount))) {
+            let initialLoan = 0;
+            if (showYear) {
+                initialLoan = allValues.eventualLoan * (1 - allValues.term * (allValues.rate / 100));
+            }
+            else {
+                initialLoan = allValues.eventualLoan * (1 - (allValues.daysAmount / allValues.yearDaysAmount) * (allValues.rate / 100));
+            }
             const discount = allValues.eventualLoan - initialLoan;
             form.setFieldsValue({ initialLoan: round(initialLoan, 2), discount: round(discount, 2) });
         }
-    }, [form]);
+    }, [form, showYear]);
+
+    const onRadioChange = useCallback((e) => {
+        e.target.value === 1 ? setShowYear(true) : setShowYear(false);
+    }, []);
 
     return (
         <div>
@@ -38,7 +51,7 @@ const BankAccounting: FC = () => {
                     gutter={[16, 0]}
                     className="row-without-margin"
                 >
-                    <Col span={5}>
+                    <Col span={8}>
                         <Form.Item
                             name="eventualLoan"
                             label="Нарощена сума (S)"
@@ -46,7 +59,7 @@ const BankAccounting: FC = () => {
                             <InputNumber placeholder="100000" />
                         </Form.Item>
                     </Col>
-                    <Col span={5}>
+                    <Col span={8}>
                         <Form.Item
                             name="rate"
                             label="Облікова ставка (d) %"
@@ -55,24 +68,65 @@ const BankAccounting: FC = () => {
                         </Form.Item>
                     </Col>
 
-                    <Col span={5}>
-                        <Form.Item
-                            name="term"
-                            label="Термін угоди у роках (n)"
-                        >
-                            <InputNumber placeholder="1" />
-                        </Form.Item>
-                    </Col>
-
                 </Row>
 
                 <Row
                     justify="start"
-                    align="top"
+                    align="bottom"
+                    gutter={[16, 0]}
+                    className="row-without-margin year-select-block"
+                >
+                    <Radio.Group onChange={onRadioChange} defaultValue={1}>
+                        <Radio value={1}>Ввести кількість років</Radio>
+                        <Radio value={2}>Ввести кількість днів</Radio>
+                    </Radio.Group>
+                </Row>
+
+                <Row
+                    justify="start"
+                    align="bottom"
                     gutter={[16, 0]}
                     className="row-without-margin"
                 >
-                    <Col span={5}>
+                    {
+                        showYear ?
+                            <Col span={8}>
+                                <Form.Item
+                                    name="term"
+                                    label="Термін угоди у роках (n)"
+                                >
+                                    <InputNumber placeholder="1" />
+                                </Form.Item>
+                            </Col>
+                            :
+                            <>
+                                <Col span={8}>
+                                    <Form.Item
+                                        name="daysAmount"
+                                        label="Кількість днів позики (t)"
+                                    >
+                                        <InputNumber placeholder="360" />
+                                    </Form.Item>
+                                </Col><Col span={8}>
+                                    <Form.Item
+                                        name="yearDaysAmount"
+                                        label="Кількість днів у році (K)"
+                                    >
+                                        <InputNumber placeholder="360" />
+                                    </Form.Item>
+                                </Col>
+                            </>
+                    }
+                </Row>
+
+
+                <Row
+                    justify="start"
+                    align="bottom"
+                    gutter={[16, 0]}
+                    className="row-without-margin"
+                >
+                    <Col span={8}>
                         <Form.Item
                             name="initialLoan"
                             label="Cума грошей, що даються в борг (P)"
@@ -80,7 +134,7 @@ const BankAccounting: FC = () => {
                             <InputNumber disabled />
                         </Form.Item>
                     </Col>
-                    <Col span={5}>
+                    <Col span={8}>
                         <Form.Item
                             name="discount"
                             label="Дисконт (D)"
--- src/modules/Task1/DiscountingAndAccounting/MathematicalDiscounting.tsx
+++ src/modules/Task1/DiscountingAndAccounting/MathematicalDiscounting.tsx
@@ -1,12 +1,14 @@
-import React, { FC, useCallback } from "react";
-import { Col, Form, InputNumber, Row } from "antd";
+import React, { FC, useCallback, useState } from "react";
+import { Col, Form, InputNumber, Radio, Row } from "antd";
 
 import { round } from '../../../utils/helpers';
 
-export interface MathematicalDiscountingProps {
+export interface MathematicalDiscountingProps { // completely done
     initialLoan: number;
     rate: number;
     term: number;
+    daysAmount: number;
+    yearDaysAmount: number;
     paymentCount: number;
     eventualLoan: number;
     discount: number;
@@ -14,14 +16,25 @@ export interface MathematicalDiscountingProps {
 
 const MathematicalDiscounting: FC = () => {
     const [form] = Form.useForm<MathematicalDiscountingProps>();
+    const [showYear, setShowYear] = useState<boolean>(true);
 
-    const handleChange = useCallback((_, allValues: MathematicalDiscountingProps) => { //TODO : calculate years by date
-        if (allValues.eventualLoan && allValues.rate && allValues.term) {
-            const initialLoan = allValues.eventualLoan / (1 + allValues.term * (allValues.rate / 100));
-            const discount = initialLoan * allValues.term * (allValues.rate / 100);
+    const handleChange = useCallback((_, allValues: MathematicalDiscountingProps) => {
+        if (allValues.eventualLoan && allValues.rate && (allValues.term || (allValues.daysAmount && allValues.yearDaysAmount))) {
+            let initialLoan = 0;
+            if (showYear) {
+                initialLoan = allValues.eventualLoan / (1 + allValues.term * (allValues.rate / 100));
+            }
+            else {
+                initialLoan = allValues.eventualLoan / (1 + (allValues.daysAmount / allValues.yearDaysAmount) * (allValues.rate / 100));
+            }
+            const discount = allValues.eventualLoan - initialLoan;
             form.setFieldsValue({ initialLoan: round(initialLoan, 2), discount: round(discount, 2) });
         }
-    }, [form]);
+    }, [form, showYear]);
+
+    const onRadioChange = useCallback((e) => {
+        e.target.value === 1 ? setShowYear(true) : setShowYear(false);
+    }, []);
 
     return (
         <div>
@@ -38,7 +51,7 @@ const MathematicalDiscounting: FC = () => {
                     gutter={[16, 0]}
                     className="row-without-margin"
                 >
-                    <Col span={5}>
+                    <Col span={8}>
                         <Form.Item
                             name="eventualLoan"
                             label="Нарощена сума (S)"
@@ -46,7 +59,7 @@ const MathematicalDiscounting: FC = () => {
                             <InputNumber placeholder="100" />
                         </Form.Item>
                     </Col>
-                    <Col span={5}>
+                    <Col span={8}>
                         <Form.Item
                             name="rate"
                             label="Відсоткова ставка (і) %"
@@ -55,24 +68,64 @@ const MathematicalDiscounting: FC = () => {
                         </Form.Item>
                     </Col>
 
-                    <Col span={5}>
-                        <Form.Item
-                            name="term"
-                            label="Термін угоди у роках (n)"
-                        >
-                            <InputNumber placeholder="0.2486" /> 
-                        </Form.Item>
-                    </Col>
-
                 </Row>
 
                 <Row
                     justify="start"
-                    align="top"
+                    align="bottom"
+                    gutter={[16, 0]}
+                    className="row-without-margin year-select-block"
+                >
+                    <Radio.Group onChange={onRadioChange} defaultValue={1}>
+                        <Radio value={1}>Ввести кількість років</Radio>
+                        <Radio value={2}>Ввести кількість днів</Radio>
+                    </Radio.Group>
+                </Row>
+
+                <Row
+                    justify="start"
+                    align="bottom"
                     gutter={[16, 0]}
                     className="row-without-margin"
                 >
-                    <Col span={5}>
+                    {
+                        showYear ?
+                            <Col span={8}>
+                                <Form.Item
+                                    name="term"
+                                    label="Термін угоди у роках (n)"
+                                >
+                                    <InputNumber placeholder="0.2486" />
+                                </Form.Item>
+                            </Col>
+                            :
+                            <>
+                                <Col span={8}>
+                                    <Form.Item
+                                        name="daysAmount"
+                                        label="Кількість днів позики (t)"
+                                    >
+                                        <InputNumber placeholder="91" />
+                                    </Form.Item>
+                                </Col><Col span={8}>
+                                    <Form.Item
+                                        name="yearDaysAmount"
+                                        label="Кількість днів у році (K)"
+                                    >
+                                        <InputNumber placeholder="366" />
+                                    </Form.Item>
+                                </Col>
+                            </>
+                    }
+                </Row>
+
+                <Row
+                    justify="start"
+                    align="bottom"
+                    gutter={[16, 0]}
+                    className="row-without-margin"
+                >
+                    <Col span={8}>
                         <Form.Item
                             name="initialLoan"
                             label="Cума грошей, що даються в борг (P)"
@@ -80,7 +133,7 @@ const MathematicalDiscounting: FC = () => {
                             <InputNumber disabled />
                         </Form.Item>
                     </Col>
-                    <Col span={5}>
+                    <Col span={8}>
                         <Form.Item
                             name="discount"
                             label="Дисконт (D)"
